80bd682443468140cec1c69980cd013e47663185
Turcsa Alexandru <aturcsa@gmail.com>
2023-10-27 11:52:40 +0300

    Lab4

---
+++ src/HAL9000/semaphore.c
@@ -0,0 +1,29 @@
+#include "HAL9000.h"
+#include "thread_internal.h"
+#include "mutex.h"
+
+
+typedef struct _SEMAPHORE
+{
+    DWORD           Value;
+
+    // ... add more fields here ...
+} SEMAPHORE, * PSEMAPHORE;
+
+void
+SemaphoreInit(
+    OUT     PSEMAPHORE      Semaphore,
+    IN      DWORD           InitialValue
+);
+
+void
+SemaphoreDown(
+    INOUT   PSEMAPHORE      Semaphore,
+    IN      DWORD           Value
+);
+
+void
+SemaphoreUp(
+    INOUT   PSEMAPHORE      Semaphore,
+    IN      DWORD           Value
+);
